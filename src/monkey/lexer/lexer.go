d75dc3f9785399efe6f42bb9feaf9dd22d443aac
cool4zbl <binliu.zhang@gmail.com>
2023-09-04 23:01:31 +0200

    add more functions in lexer.go

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "monkey/token"
+
 type Lexer struct {
 	input        string
 	position     int  // current position in input (points to current char)
@@ -9,5 +11,87 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
+
 	return l
+}
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		// set ending flat `0` of the input
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		// if is a letter, read the rest of the identifier/keyword until it encounters a non-letter-char
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+
+	// advance the pointers into the input before returning the token,
+	// so when we call `NextToken()` again the `l.ch` field is already updated.
+	l.readChar()
+	return tok
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+// reads in an identifier and advances lexer's positions until it encounters a non-letter-character
+func (l *Lexer) readIdentifier() string {
+	pos := l.position
+
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	println("input: ", l.input[pos:l.position])
+	return l.input[pos:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\n' || l.ch == '\r' || l.ch == '\t' {
+		l.readChar()
+	}
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{
+		Type:    tokenType,
+		Literal: string(ch),
+	}
 }
--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -1,9 +1,5 @@
 package token
 
-// Token represents a token type and its literal value.
-
-type TokenType string
-
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
@@ -29,9 +25,41 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	RETURN   = "RETURN"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	BANG     = "!"
+	MINUS    = "-"
+	SLASH    = "/"
+	ASTERISK = "*"
+
+	LT = "<"
+	GT = ">"
 )
 
+type TokenType string
+
+// Token represents a token type and its literal value.
 type Token struct {
 	Type    TokenType
 	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
